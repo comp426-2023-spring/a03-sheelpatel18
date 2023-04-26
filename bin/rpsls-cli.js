788ca69b42ad543630d1ae5b167a5ec979115488
Sheel Patel <sheel@letswhirl.com>
2023-04-26 14:44:31 -0400

    latest

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import minimist from 'minimist';
+import {rpsls} from './rpsls.js'
 
 const args = minimist(process.argv.slice(2));
 
@@ -32,110 +33,5 @@ if (args.r) {
     - Rock CRUSHES Scissors`)
 }
 
-export const rps = (opMove) => {
-    const moves = ['rock', 'paper', 'scissors', 'lizard', 'spock']
-    const randomMove = moves[Math.floor(Math.random() * moves.length)]
-    if (!opMove) {
-        return {"player":randomMove}
-    } else {
-        switch (randomMove) {
-            case 'rock':
-                if (opMove === 'paper') {
-                    return {"player":randomMove, "opponent":opMove, "result":"lose"}
-                }
-                if (opMove === 'scissors') {
-                    return {"player":randomMove, "opponent":opMove, "result":"win"}
-                }
-                if (opMove === 'rock') {
-                    return {"player":randomMove, "opponent":opMove, "result":"tie"}
-                }
-                if (opMove === 'lizard') {
-                    return {"player":randomMove, "opponent":opMove, "result":"win"}
-                }
-                if (opMove === 'spock') {
-                    return {"player":randomMove, "opponent":opMove, "result":"lose"}
-                }
-            case 'paper':
-                if (opMove === 'scissors') {
-                    return {"player":randomMove, "opponent":opMove, "result":"lose"}
-                }
-                if (opMove === 'rock') {
-                    return {"player":randomMove, "opponent":opMove, "result":"win"}
-                }
-                if (opMove === 'paper') {
-                    return {"player":randomMove, "opponent":opMove, "result":"tie"}
-                }
-                if (opMove === 'lizard') {
-                    return {"player":randomMove, "opponent":opMove, "result":"lose"}
-                }
-                if (opMove === 'spock') {
-                    return {"player":randomMove, "opponent":opMove, "result":"win"}
-                }
-            case 'scissors':
-                if (opMove === 'rock') {
-                    return {"player":randomMove, "opponent":opMove, "result":"lose"}
-                }
-                if (opMove === 'paper') {
-                    return {"player":randomMove, "opponent":opMove, "result":"win"}
-                }
-                if (opMove === 'scissors') {
-                    return {"player":randomMove, "opponent":opMove, "result":"tie"}
-                }
-                if (opMove === 'lizard') {
-                    return {"player":randomMove, "opponent":opMove, "result":"win"}
-                }
-                if (opMove === 'spock') {
-                    return {"player":randomMove, "opponent":opMove, "result":"lose"}
-                }
-            case 'lizard':
-                if (opMove === 'rock') {
-                    return {"player":randomMove, "opponent":opMove, "result":"lose"}
-                }
-                if (opMove === 'paper') {
-                    return {"player":randomMove, "opponent":opMove, "result":"win"}
-                }
-                if (opMove === 'scissors') {
-                    return {"player":randomMove, "opponent":opMove, "result":"lose"}
-                }
-                if (opMove === 'lizard') {
-                    return {"player":randomMove, "opponent":opMove, "result":"tie"}
-                }
-                if (opMove === 'spock') {
-                    return {"player":randomMove, "opponent":opMove, "result":"win"}
-                }
-            case 'spock':
-                if (opMove === 'rock') {
-                    return {"player":randomMove, "opponent":opMove, "result":"win"}
-                }
-                if (opMove === 'paper') {
-                    return {"player":randomMove, "opponent":opMove, "result":"lose"}
-                }
-                if (opMove === 'scissors') {
-                    return {"player":randomMove, "opponent":opMove, "result":"win"}
-                }
-                if (opMove === 'lizard') {
-                    return {"player":randomMove, "opponent":opMove, "result":"lose"}
-                }
-                if (opMove === 'spock') {
-                    return {"player":randomMove, "opponent":opMove, "result":"tie"}
-                }
-            default:
-                console.error(`${opMove} is out of range.`)
-                console.log(`Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
 
-                - Scissors CUTS Paper
-                - Paper COVERS Rock
-                - Rock SMOOSHES Lizard
-                - Lizard POISONS Spock
-                - Spock SMASHES Scissors
-                - Scissors DECAPITATES Lizard
-                - Lizard EATS Paper
-                - Paper DISPROVES Spock
-                - Spock VAPORIZES Rock
-                - Rock CRUSHES Scissors`)
-                break;
-        }
-    }
-}
-
-console.log(rps(args._[0]))
+console.log(rpsls(args._[0]))
