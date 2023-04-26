ba81df943a365d0c05d69d4aa73bd8c75266c030
Sheel Patel <sheel@letswhirl.com>
2023-04-26 00:42:12 -0400

    new cli features

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -3,11 +3,33 @@ import minimist from 'minimist';
 
 const args = minimist(process.argv.slice(2));
 
+// if -h is passed, console.log "TEST"
+if (args.h) {
+    console.log(`Usage: node-rps [SHOT]
+    Play Rock Paper Scissors (RPS)
+    
+      -h, --help      display this help message and exit
+      -r, --rules     display the rules and exit
+    
+    Examples:
+      node-rps        Return JSON with single player RPS result.
+                      e.g. {"player":"rock"}
+      node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                      e.g {"player":"rock","opponent":"scissors","result":"win"}`)
+}
+
+if (args.r) {
+    console.log(`Rules for Rock Paper Scissors:
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`)
+}
+
 export const rps = (opMove) => {
     const moves = ['rock', 'paper', 'scissors']
     const randomMove = moves[Math.floor(Math.random() * moves.length)]
     if (!opMove) {
-        return {"player":randomMove}
+        return {'player':randomMove}
     } else {
         switch (randomMove) {
             case 'rock':
